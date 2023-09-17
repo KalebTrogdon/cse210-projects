9576a2b22c937aca73e8ceb5a56f549215105cab
Kaleb Trogdon <67351277+KalebTrogdon@users.noreply.github.com>
2023-09-16 20:58:23 -0600

    Added Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,61 @@
 using System;
 
-class Program
+namespace Prep2
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep2 World!");
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your grade percentage: ");
+            string input = Console.ReadLine();
+
+            int percentage = int.Parse(input);
+            string letter;
+            
+            if (percentage >= 90)
+            {
+                letter = "A";
+            }
+            else if (percentage >= 80)
+            {
+                letter = "B";
+            }
+            else if (percentage >= 70)
+            {
+                letter = "C";
+            }
+            else if (percentage >= 60)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "F";
+            }
+
+            if (percentage >= 70)
+            {
+                Console.WriteLine("Congratulations! You passed the course.");
+            }
+            else
+            {
+                Console.WriteLine("Better luck next time. Keep working hard.");
+            }
+
+            string sign = "";
+
+            if (percentage % 10 >= 7)
+            {
+                sign = "+";
+            }
+            else if (percentage % 10 < 3)
+            {
+                sign = "-";
+            }
+
+            string finalGrade = letter + sign;
+
+            Console.WriteLine($"Your final grade is {finalGrade}");
+        }
     }
 }
